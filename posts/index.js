68575e5a3de1aa55b2eaa8d100da6350e9ed3458
Piotr Zając <piotr.zajac@live.com>
2019-03-19 11:19:53 +0100

    listen to file changes with chokidar

--- posts/index.js
+++ posts/index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const cors = require('cors')
-const fs = require("fs");
+const chokidar = require("chokidar")
+const fs = require('fs')
 const app = express()
 
-const posts = JSON.parse(fs.readFileSync('./fakes/posts.json').toString())
+function loadJSON(filename){
+    return JSON.parse(fs.readFileSync(filename).toString())
+}
+
+let posts = loadJSON('./fakes/posts.json')
+chokidar.watch('./fakes').on('all', () => {
+    posts = loadJSON('./fakes/posts.json')
+})
 
 app.use(cors())
 app.get('/posts', (req, res) => res.json(posts))
