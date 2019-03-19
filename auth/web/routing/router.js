0ac3a8c10233ca13b8de63a388d2b6c5e5701121
piotrzajac <piotr.zajac@live.com>
2019-03-19 23:40:58 +0100

    crypto

--- auth/web/routing/router.js
+++ auth/web/routing/router.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const crypto = require('crypto')
 const chokidar = require("chokidar")
 const fs = require('fs')
 
 const router = express.Router()
+
+const salt = 'piu2h34piuhdfsipuhiuwheru2n34kjndsoij';
+
+function encode(text) {
+    return crypto.createHmac('sha256', salt)
+        .update(text)
+        .digest('hex');
+}
 
 function loadJSON(filename){
     return JSON.parse(fs.readFileSync(filename).toString())
@@ -20,9 +29,8 @@ router.post('/', (req, res) => {
     })
 
     res.status(isAuthorized ? 200 : 401)
-    res.json({ isAuthorized: isAuthorized })
+    res.json({ status: isAuthorized })
 })
-router.get('*', (req, res) => res.end('error'))
 
 module.exports = (app) => {
     app.use(router)
